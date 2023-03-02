3e11912a0f2bc84f83f08eca28444bf4d68acd23
YohabPark <dyqkr@naver.com>
2023-03-02 22:48:06 +0900

    Add long wav

--- app.py
+++ app.py
@@ -6,13 +6,62 @@ import speech_recognition as sr
 from googletrans import Translator, constants
 from pprint import pprint
 from moviepy.editor import *
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    r = sr.Recognizer()
+
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)  
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk 
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+
 def video_to_translate(file_obj,initial_language,final_language):
 # Insert Local Video File Path
     videoclip = VideoFileClip(file_obj.name)
     # Insert Local Audio File Path
     videoclip.audio.write_audiofile("test.wav",codec='pcm_s16le')
 # initialize the recognizer
-    r = sr.Recognizer()
+   # r = sr.Recognizer()
 
     if initial_language == "English":
         lang_in='en-US'
@@ -27,11 +76,13 @@ def video_to_translate(file_obj,initial_language,final_language):
     elif initial_language == "Japanese":
         lang_in='ja-JP'
     # open the file
-    with sr.AudioFile("test.wav") as source:
-        # listen for the data (load audio to memory)
-        audio_data = r.record(source)
-        # recognize (convert from speech to text)
-        text = r.recognize_google(audio_data, language = lang_in)
+    #with sr.AudioFile("test.wav") as source:
+    #    # listen for the data (load audio to memory)
+    #    audio_data = r.record(source)
+    #    # recognize (convert from speech to text)
+    #    text = r.recognize_google(audio_data, language = lang_in)
+
+    text = get_large_audio_transcription("test.wav")
 
     if final_language == "English":
         lang='en'
@@ -44,7 +95,7 @@ def video_to_translate(file_obj,initial_language,final_language):
     elif final_language == "German":
         lang='de'
     elif final_language == "Japanese":
-        lang='ja'               
+        lang='ko'               
     print(lang)
     # init the Google API translator
     translator = Translator()
